f8c1aa6f2a7ad01893ac908313b44b957aca039e
Antonio Skecheliev <147728161+Antonioskech@users.noreply.github.com>
2025-04-28 23:12:41 +0300

    Update Conway's_game_of_life.cpp

--- Conway's_game_of_life/Conway's_game_of_life/Conway's_game_of_life.cpp
+++ Conway's_game_of_life/Conway's_game_of_life/Conway's_game_of_life.cpp
@@ -1,16 +1,3 @@
-/**
-*
-* Solution to course project # <2>
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2023/2024
-*
-* @author <Antonio Skecheliev>
-* @idnumber <1MI0600276>
-* @compiler <VC>
-*
-*/
-
 #include <iostream>
 #include <fstream>
 #include <cmath>
